1fede677024841980b4f5fa5eeeadbcc60f26acb
sjlee3157 <sjlee3157@gmail.com>
2018-12-13 15:50:36 -0800

    Change SearchBar to class component and controlled form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SearchBar.css';
 
-const SearchBar = (props) => {
-  const onChangeHandler = (e) => {
-    let query = e.target.value;
-    props.searchPetsCallback(query);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: undefined
+    };
   }
 
-  const handleSubmit = (e) => {
+  onChangeHandler = (e) => {
+    const query = e.target.value;
+    this.setState({ query })
+    this.props.searchPetsCallback(query);
+  }
+
+  handleSubmit = (e) => {
     e.preventDefault();
   }
 
-  return (
-    <form onSubmit={ handleSubmit }>
+  render() {
+    return (
+    <form onSubmit={ this.handleSubmit }>
       <input
         className="search-bar"
         placeholder="Filter Pets"
-        onChange={ onChangeHandler } />
+        onChange={ this.onChangeHandler } />
     </form>
-  );
-};
+    );
+  }
+}
 
 SearchBar.propTypes = {
   searchPetsCallback: PropTypes.func.isRequired
